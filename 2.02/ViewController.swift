2d39d42f8c9c517d20011c5fff385c80719fc425
TeilorVladimir <giever2222@gmail.com>
2023-03-18 17:40:33 +0300

    Initial Commit

---
+++ 2.02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.02
+//
+//  Created by Сораб В Тейлор on 18.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
