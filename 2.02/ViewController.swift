c08a5ff6df411fb4cb4515d5a019305f2959e9d6
TeilorVladimir <giever2222@gmail.com>
2023-03-19 18:13:44 +0300

    make a kind of a circle

--- 2.02/ViewController.swift
+++ 2.02/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redSemaforColor: UIView!
+    
+    @IBOutlet var yellowSemaforColor: UIView!
+    
+    @IBOutlet var greenSemaforColor: UIView!
+    
+    @IBOutlet var semaforButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSemaforColor.layer.cornerRadius = 66
+        yellowSemaforColor.layer.cornerRadius = 66
+        greenSemaforColor.layer.cornerRadius = 66
+        
+        
+        semaforButton.layer.cornerRadius = 30
+        semaforButton.setTitle("Start", for: .normal)
+    }
+    
+    @IBAction func actionButton(_ sender: UIButton) {
+        
+        semaforButton.setTitle("Next", for: .normal)
+        
+            if redSemaforColor.alpha == 1 {
+                redSemaforColor.alpha = 0.4
+                yellowSemaforColor.alpha = 1
+            }
+            else if yellowSemaforColor.alpha == 1 {
+                yellowSemaforColor.alpha = 0.4
+                greenSemaforColor.alpha = 1
+                
+            }
+            else if greenSemaforColor.alpha == 1 {
+                greenSemaforColor.alpha = 0.4
+                redSemaforColor.alpha = 1
+            }
+        }
     }
 
 
-}
+    
+
+
+
+
 
